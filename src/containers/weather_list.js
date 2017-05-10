655eda2ddacdfb9532a05778f8d8097609fcd1e6
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-10 15:28:01 +0200

    Add new container weather list and connects to the store

---
+++ src/containers/weather_list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  
+  render() {
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          
+        </tbody>
+      </table>
+
+    );
+
+  }
+}
+
+function mapStateToProps({weather}){
+  return {weather};
+}
+
+// It's null because there not mapStateToProps yet
+export default connect(mapStateToProps)(WeatherList);
