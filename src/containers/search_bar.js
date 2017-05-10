d504e36c394d370e7d9716cc87655dd8b504df5e
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-10 11:09:20 +0200

    Improve input search bar and bind the react context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
+
+    // Without this the `this` inside of the function
+    // wont be the `this` of the react class
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+    this.setState({term: event.target.value});
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+           placeholder="Get five day forcast in your favorite cities"
+           className="form-control"
+           onChange={this.onInputChange}
+           value={this.state.term}
+           />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
