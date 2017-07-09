c73475bbc0a701ee28475bff725cdfd9ef1865fb
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-09 12:32:58 +0200

    Fix typo

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-export const FETCH_WEATHER = 'FETCH_WEATHERE';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`;
